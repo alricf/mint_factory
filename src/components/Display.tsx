af700cbe56e7069874904f5e9154ba83204c0a45
alricf <alricfernandes11@gmail.com>
2024-09-26 13:26:48 -0400

    Adding NFT modal display

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,13 +1,24 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import { pinata } from "../../utils/config";
+import { useState } from 'react';
 import Image from 'next/image';
 
 interface DisplayProps {
   allData: object[] | null;
 }
+
 const Display: React.FC<DisplayProps> = ({ allData }) => {
-  console.log(allData, 'allData');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImageUrl, setSelectedImageUrl] = useState<string>('');
+
+  const openModal = (url: string) => {
+    setSelectedImageUrl(url);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedImageUrl('');
+  };
 
   return (
     <div className="bg-[#11F9ED] p-6 border-y-black border-y-4">
@@ -27,7 +38,7 @@ const Display: React.FC<DisplayProps> = ({ allData }) => {
                 width={400}
                 height={400}
                 className="w-full rounded-md cursor-pointer"
-                onClick={() => window.location.href = `https://${process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL}/${element.result}`}
+                onClick={() => openModal(`https://${process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL}/${element.result}`)}
               />
               <div className="mt-4 text-center">
                 <h2 className="text-xl font-semibold text-white">{element.name}</h2>
@@ -38,8 +49,37 @@ const Display: React.FC<DisplayProps> = ({ allData }) => {
         ) : (
           <div className="col-span-1 sm:col-span-2 md:col-span-3">
             <p className="text-center text-gray-900 text-2xl">0 Assets Owned</p>
-          </div>)}
+          </div>
+        )}
       </div>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+          onClick={closeModal}
+        >
+          <div
+            className="relative bg-white p-4 rounded-lg shadow-lg"
+            // Stop on click action from closing modal
+            onClick={(e) => e.stopPropagation()}
+          >
+            <span
+              className="absolute top-2 right-2 text-2xl font-bold cursor-pointer text-red-500"
+              onClick={closeModal}
+            >
+              &times;
+            </span>
+            <Image
+              src={selectedImageUrl}
+              alt="Modal Image"
+              width={500}
+              height={500}
+              className="rounded-lg object-contain"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
