911207852d213d8bbc50fedf2ceefa3653d8fd88
alricf <alricfernandes11@gmail.com>
2024-11-11 11:48:21 -0500

    Add route to upload NFT files to pinata

---
+++ src/app/api/file/create/route.ts
@@ -0,0 +1,59 @@
+import { NextResponse, NextRequest } from "next/server";
+import { pinata } from "../../../../../utils/config";
+import { ethers } from 'ethers';
+import { v4 as uuidv4 } from 'uuid';
+
+export const configuration = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(request: NextRequest) {
+  try {
+    // Gather data
+    const data = await request.formData();
+
+    const name: string = data.get('name') as string;
+    const description: string = data.get('description') as string;
+    const files: File[] = data.getAll('files[]') as File[];
+
+    const uploadedMetadataFileBaseURIs: string[] = [];
+
+    for (const file of files) {
+      const fileExtension = file.name.split('.').pop();
+
+      const uniqueId = uuidv4();
+
+      // Upload NFT file to pinata
+      const uploadFileData = await pinata.upload
+        .file(file)
+        .addMetadata({
+          name: `${uniqueId}.${fileExtension}`
+        });
+
+      // Upload json file to pinata
+      const uploadMetadata = await pinata.upload
+        .json({
+          name,
+          description,
+          image: `ipfs/${uploadFileData.IpfsHash}/${uniqueId}.${fileExtension}`,
+        })
+        .addMetadata({
+          name: `${uniqueId}.json`
+        });
+
+      uploadedMetadataFileBaseURIs.push(uploadMetadata.IpfsHash);
+    }
+
+    return NextResponse.json({
+      uploadedMetadataFileBaseURIs
+    }, { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
