ce652e86347a70b58c38fd5895bb885c637d63b4
alricf <alricfernandes11@gmail.com>
2024-11-12 12:14:13 -0500

    Refactor NFT minting flow to support multiple NFTs, improved file handling, and efficient transaction processing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 require('dotenv').config();
-
 import './globals.css';
 import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
 import { ethers } from 'ethers';
-
 import MINT_FACTORY_ABI from '../abis/MintFactory.json';
 import config from '../config.json';
 
@@ -13,22 +11,26 @@ import Navigation from '@/components/Navigation';
 import Create from '@/components/Create';
 import Display from '@/components/Display';
 
-// Wallet connection libraries
+// Wallet connection library
 import Web3Modal from 'web3modal';
 
 export default function Home() {
-  const [files, setFiles] = useState<FileList | null>(null);
-  const [name, setName] = useState<String | null>(null);
-  const [description, setDescription] = useState<String | null>(null);
+  const [nfts, setNFTs] = useState<{
+    name: string;
+    description: string;
+    files: File[];
+    blobURLs: string[];
+  }[]>([]);
   const [account, setAccount] = useState<string | null>(null);
   const [provider, setProvider] = useState<any | null>(null);
   const [signature, setSignature] = useState<string | null>(null);
   const [message, setMessage] = useState<string | null>(null);
   const [allData, setAllData] = useState<object[] | null>(null);
+  const [totalSupply, setTotalSupply] = useState<number | null>(null);
 
   const read = async () => {
     if (account !== null) {
-      // Sending files to server
+      // Sending account to server
       const response = await fetch('/api/file/read', {
         method: 'POST',
         headers: {
@@ -37,19 +39,12 @@ export default function Home() {
         body: JSON.stringify({ account }),
       });
       const data = await response.json();
-      console.log('Data received from server:', data);
-      console.log('Type of data:', typeof data);
-
-      // Log the structure of the data
-      if (typeof data === 'object') {
-        console.log('Data keys:', Object.keys(data));
-      }
 
       // Check if data contains allData
       if (Array.isArray(data)) {
-        setAllData(data); // Direct array
+        setAllData(data);
       } else if (data.allData && Array.isArray(data.allData)) {
-        setAllData(data.allData); // Extract from object
+        setAllData(data.allData);
       } else {
         console.error('Expected an array but received:', data);
       }
@@ -58,15 +53,14 @@ export default function Home() {
 
   useEffect(() => {
     read();
-  }, [account]);
-  console.log(allData);
+  }, [account, totalSupply]);
 
   // Wallet connect handler
   const connectWallet = async () => {
     try {
       const web3Modal = new Web3Modal({
-        cacheProvider: false, // optional
-        providerOptions: {},  // MetaMask doesn't require extra config
+        cacheProvider: false,
+        providerOptions: {},
       });
 
       const instance = await web3Modal.connect(); // MetaMask prompt
@@ -75,11 +69,9 @@ export default function Home() {
       setProvider(provider);
 
       const signer = await provider.getSigner();
-      // setSigner(signer)
 
-      const address = await signer.getAddress(); // user's address
-      setAccount(address); // Set the connected user's account address
-      console.log("Connected to:", address);
+      const address = await signer.getAddress();
+      setAccount(address);
 
       // Sign a message for proof of intent
       const message = `${address} is minting NFT(s) using Mint Factory`;
@@ -104,95 +96,103 @@ export default function Home() {
     setMessage(null);
     setSignature(null);
     setAllData(null);
-    console.log("Wallet Disconnected");
   };
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { id, value } = event.target;
+  const handleChange = (index: number, event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value, files } = event.target as HTMLInputElement;
+    const newNFTs = [...nfts];
 
-    // Handle Form Inputs
-    if (id === 'formNFTName') setName(value);
-    if (id === 'formNFTDescription') setDescription(value);
-    if (event.target instanceof HTMLInputElement && event.target.type === 'file' && event.target.files) {
-      setFiles(event.target.files);
+    // Handle file changes
+    if (event.target.type === 'file' && files) {
+      const filesArray = Array.from(files);
+
+      const newBlobURLs = filesArray.map((file) => URL.createObjectURL(file));
+
+      newNFTs[index] = {
+        ...newNFTs[index],
+        files: filesArray.length > 0 ? filesArray : [],
+        blobURLs: newBlobURLs,
+      };
+    } else {
+      newNFTs[index] = {
+        ...newNFTs[index],
+        [name]: value,
+      };
     }
+
+    setNFTs(newNFTs);
   };
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
-    if (!provider) alert('Please connect your wallet before minting');
-    if (!files) {
-      alert('Please select a file first!');
+    if (!provider) {
+      alert('Please connect your wallet before minting');
       return;
     }
 
-    const formData = new FormData();
-    formData.append('name', name as string);
-    formData.append('description', description as string);
-    Array.from(files).forEach((file) => {
-      formData.append('files', file);
-    });
-    formData.append('account', account as string);
-    formData.append('signature', signature as string);
-    formData.append('message', message as string);
-
     try {
-      // Sending files to server
-      const response = await fetch('/api/file/create', {
-        method: 'POST',
-        body: formData,
+      let metadataBaseURIs: string[] = [];
+
+      // Collect promises from asynchronous operations
+      const promises = nfts.map(async (nft, index) => {
+        const nftFormData = new FormData();
+
+        nftFormData.append('name', nft.name);
+        nftFormData.append('description', nft.description);
+
+        nft.files.forEach((file) => {
+          nftFormData.append('files[]', file);
+        });
+
+        const response = await fetch('/api/file/create', {
+          method: 'POST',
+          body: nftFormData,
+        });
+
+        if (!response.ok) throw new Error(`Server error: ${response.status}`);
+
+        const data = await response.json();
+        console.log(data, 'dataFE');
+        console.log(data.uploadedMetadataFileBaseURI, 'data.uploadedMetadataFileBaseURI');
+
+        const baseURIs = data.uploadedMetadataFileBaseURIs.flatMap((uri: string) => {
+          return uri;
+        });
+        return baseURIs;
       });
 
-      if (!response.ok) throw new Error(`Server error: ${response.status}`);
+      setNFTs([]);
 
-      const data = await response.json();
-      console.log(data, 'data');
+      // Wait for all asynchronous operations to complete
+      const promisesCollection = await Promise.all(promises);
 
-      console.log(data.typeof, 'data typeof');
-
-      const metadataBaseURIs = data.uploadedMetadataFiles.map(element => {
-        return element;
-      });
-      console.log(metadataBaseURIs, 'metadataBaseURIs');
+      metadataBaseURIs = promisesCollection.flat();
 
       const signer2 = await provider.getSigner();
-      console.log(signer2, 'signer2');
-
-      // MintFactory contract interaction
       const mintFactory = new ethers.Contract(
         config[31337].mintFactory.address,
         MINT_FACTORY_ABI,
         signer2
       );
-      console.log(mintFactory, 'mintFactory');
-      console.log(metadataBaseURIs.length, 'metadataBaseURIs.length', metadataBaseURIs, 'metadataBaseURIs');
 
-      // Sending the right amount of Ether from the user's wallet
+      let totalSupply = await mintFactory.totalSupply();
+      setTotalSupply(totalSupply);
+
+      // Send the minting transaction
       const tx = await mintFactory.mint(metadataBaseURIs.length, metadataBaseURIs, {
-        value: ethers.parseUnits(metadataBaseURIs.length.toString(), "ether"),
+        value: ethers.parseUnits(metadataBaseURIs.length.toString(), 'ether'),
       });
-      console.log(tx);
+      await tx.wait();
 
-      await tx.wait(); // Wait for transaction to be mined
-      console.log("Minting successful:", tx.hash);
-      const totalSupply = await mintFactory.totalSupply();
-      console.log(totalSupply.toString(), 'totalSupply');
-
-      // Reset form state variables
-      setName(null);
-      setDescription(null);
-      setFiles(null);
-
-      // Reset form
-      const form = event.target as HTMLFormElement;
-      form.reset();
-
-      // Refresh assets owned after minting
-      await read();
-
+      totalSupply = await mintFactory.totalSupply();
+      setTotalSupply(totalSupply);
     } catch (error) {
-      console.error('Error sending file to server:', error);
+      console.error('Error sending files to server:', error);
     }
+  };
+
+  const addNFT = (newNFT: { name: string; description: string; files: File[]; blobURLs: string[]; }) => {
+    setNFTs([...nfts, newNFT]);
   };
 
   return (
@@ -209,6 +209,9 @@ export default function Home() {
             <Create
               handleSubmit={handleSubmit}
               handleChange={handleChange}
+              nfts={nfts}
+              addNFT={addNFT}
+              removeNFT={(index) => setNFTs(nfts.filter((_, i) => i !== index))}
               allData={allData}
               account={account}
               signature={signature}
@@ -229,4 +232,4 @@ export default function Home() {
       }
     </>
   );
-}
+};
